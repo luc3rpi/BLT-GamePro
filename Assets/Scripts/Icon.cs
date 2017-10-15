fbd45a4f9fe276e742c61fe743c29dd30d3ecb78
IceAuraDragon <32280166+IceAuraDragon@users.noreply.github.com>
2017-10-15 16:04:09 -0400

    Add files via upload

---
+++ Assets/Scripts/Icon.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Icon : MonoBehaviour, IPointerUpHandler {
+	public GameObject window;
+	public float xPos, yPos, wid, hei;
+	GameObject go;
+	public void OnPointerUp (PointerEventData eventData) 
+	{
+		//Debug.Log ("Click Count = "+eventData.clickCount);
+		if(eventData.clickCount == 2){
+			go = Instantiate (window, new Vector3 (xPos, yPos, 0), Quaternion.identity) as GameObject;
+			go.transform.localScale = new Vector3 (wid, hei, 1);
+			eventData.clickCount = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/Window.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Window : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	//Called by button attached to canvas attached to window
+	public void X(){
+		Destroy (gameObject);
+	}
+}
