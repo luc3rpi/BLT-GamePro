1d17176b0fec15e684bc6f5759737fe02c916443
Form67 <32280103+Form67@users.noreply.github.com>
2017-10-30 16:47:03 -0400

    Add files via upload

---
+++ DesktopSwitch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesktopSwitch : MonoBehaviour {
+    public Vector3 desktopLocation;
+    public BoxCollider2D colid;
+    public SpriteRenderer sprite;
+    public int thisNumber;
+	// Use this for initialization
+	void Start () {
+        colid = GetComponent<BoxCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!(thisNumber == 3) && GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Master>().desktops[thisNumber] == 0)
+        {
+            colid.enabled = false;
+            sprite.enabled = false;
+        }
+        else {
+            colid.enabled = true;
+            sprite.enabled = true;
+        }
+	}
+    private void OnMouseDown()
+    {
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>().position = desktopLocation;
+    }
+}
--- Master.cs
+++ Master.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class Master : MonoBehaviour {
     public List<Document> fileBook; //master list
+    public int[] desktops;  
     // Use this for initialization
+    private void Awake()
+    {
+        fileBook = new List<Document>();
+        desktops = new int[4];
+        desktops[0] = 0; desktops[1] = 0; desktops[2] = 0; desktops[3] = 0;
+    }
     void Start () {
 	}
 	
