ccf8d9a931fd1502047245c57a90e41e901e6aea
IceAuraDragon <32280166+IceAuraDragon@users.noreply.github.com>
2017-11-02 16:23:25 -0400

    Add files via upload

---
+++ Assets/Scripts/DoubleClick.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DoubleClick : MonoBehaviour, IPointerUpHandler
+{
+	public GameObject window;
+	public float xPos, yPos, wid, hei;
+	GameObject go;
+	public void OnPointerUp (PointerEventData eventData) 
+	{
+		//Debug.Log ("Click Count = "+eventData.clickCount);
+		if(eventData.clickCount == 2){
+			go = Instantiate (window, new Vector3 (xPos, yPos, 0), Quaternion.identity) as GameObject;
+			go.transform.localScale = new Vector3 (wid, hei, 1);
+			eventData.clickCount = 0;
+		}
+	}
+}
---
+++ Assets/Scripts/XButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnClick(){
+		Destroy (transform.parent.gameObject);
+	}
+}
