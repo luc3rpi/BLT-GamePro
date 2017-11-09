dc30ed25401a6211cf6fbd1fb7f471fa614dcf07
Sam Gould <drmcman67@yahoo.com>
2017-11-09 17:49:26 -0500

    Icon script now allows placing in reference to desktop

--- CurrentBuild/Assets/Scripts/Icon.cs
+++ CurrentBuild/Assets/Scripts/Icon.cs
@@ -8,6 +8,7 @@ public class Icon : MonoBehaviour, IPointerUpHandler {
 	public float xPos, yPos, wid, hei;
 	public GameObject go;
     public int Desktop;
+    public int location;
     public bool DesktopSwitcher;
 
 	public void OnPointerUp (PointerEventData eventData) 
@@ -16,7 +17,25 @@ public class Icon : MonoBehaviour, IPointerUpHandler {
 		if(eventData.clickCount == 1){
             if ( !go)
             {
-                go = Instantiate(window, new Vector3(xPos*40*Desktop, yPos, 0), Quaternion.identity) as GameObject;
+                switch(Desktop) {
+                    case 3:
+                        location = 0;
+                        break;
+                    case 2:
+                        location = 1;
+                        break;
+                    case 1:
+                        location = 2;
+                        break;
+                    case 0:
+                        location = 3;
+                        break;
+                    default:
+                        location = 0;
+                        break;
+
+                }
+                go = Instantiate(window, new Vector3(xPos+1*40*location, yPos, 0), Quaternion.identity) as GameObject;
                 go.transform.localScale = new Vector3(wid, hei, 1);
                 if (DesktopSwitcher) {
                     go.transform.GetChild(0).GetChild(Desktop).GetComponent<SpriteRenderer>().color = Color.red;
